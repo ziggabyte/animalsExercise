0d17bb15732aa3866ea16f0b8121189e00b0a008
ziggi <ziggi.willpower@gmail.com>
2022-01-05 13:59:53 +0100

    initial commit

---
+++ src/main/java/com/example/animalsexercise/AnimalsExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalsexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalsExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/animalsexercise/AnimalsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalsexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
